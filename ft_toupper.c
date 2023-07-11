6abae8f02e577ef7bd6468aa2d67b108576fdbc9
Giovanni Troiano <136205893+giovatro@users.noreply.github.com>
2023-07-11 20:44:42 +0930

    Add files via upload

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,34 +6,15 @@
 /*   By: gtroiano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:47:29 by gtroiano          #+#    #+#             */
-/*   Updated: 2023/07/11 18:51:58 by gtroiano         ###   ########.fr       */
+/*   Updated: 2023/07/11 20:20:11 by gtroiano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
-char	*ft_toupper(char *str)
+int	ft_toupper(int c)
 {
-	int	index;
-
-	index = 0;
-	while (str[index] != '\0')
-	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-		{
-			str[index] = str[index] - 32;
-		}
-		index++;
-	}
-	return (str);
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
 }
-/*
-int	main(void)
-{
-	char lower[] = "giovanni";
-
-	ft_toupper(lower);
-	printf("%s", lower);
-	return (0);
-}
-*/
