111fc4b1cc58650f7a457e5c0794dc4a106685e2
Giovanni Troiano <136205893+giovatro@users.noreply.github.com>
2023-07-19 15:07:34 +0930

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtroiano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 22:14:14 by gtroiano          #+#    #+#             */
+/*   Updated: 2023/07/18 22:35:20 by gtroiano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+prototypes function *ft_calloc(count, size_t n)
+{
+	if either count or n is zero or if memory allocation fails, NULL is returned
+
+	// check to ensure count * n doesn't overflow size_t type
+	// malloc 
+	
+}
